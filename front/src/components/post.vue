a1bfa9f7988d922fee2c40c8e16116bd197cd246
rockage <122590@qq.com>
2020-01-17 02:09:55 +0800

    增加旋转按钮进度20

--- front/src/components/post.vue
+++ front/src/components/post.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="edit_container">
     <el-input
-      placeholder="标题"
+      placeholder="帖子标题"
       v-model="threadsTitle">
     </el-input>
     <quill-editor
@@ -15,17 +15,7 @@
     </quill-editor>
     <el-button v-on:click="saveHtml">保存</el-button>
 
-    <input type="file" id="inputImg" @change="onInputImgChange($event)">
-
-    <select id="myselect">
-      <option value="1">webp格式</option>
-      <option value="2">jpeg格式</option>
-      <option value="3">png格式</option>
-    </select>
-    <button id="start" @click="xxx">开始转换</button>
-    <p>预览：</p>
-    <img id="imgShow" src="" alt="">
-    <img id="img2" src="" alt="">
+    <input type="file" id="inputImg" @change="onInputImgChange($event)" style="display:none;">
 
   </div>
 </template>
@@ -34,33 +24,24 @@
 <script>
 
 
-  import {ImageDrop} from 'quill-image-drop-module'
+  //import {ImageDrop} from 'quill-image-drop-module'
   import ImageResize from 'quill-image-resize-module'
-  //import QuillEditor from "vue-quill-editor/src";
 
-  Quill.register('modules/imageDrop', ImageDrop)
+  //Quill.register('modules/imageDrop', ImageDrop)
   Quill.register('modules/imageResize', ImageResize)
 
   const container = [
     ['bold', 'italic', 'underline', 'strike'],
     ['blockquote', 'code-block'],
-    [{'header': 1}, {'header': 2}],
-    [{'list': 'ordered'}, {'list': 'bullet'}],
-    [{'script': 'sub'}, {'script': 'super'}],
     [{'indent': '-1'}, {'indent': '+1'}],
-    [{'direction': 'rtl'}],
     [{'size': ['small', false, 'large', 'huge']}],
-    [{'header': [1, 2, 3, 4, 5, 6, false]}],
     [{'font': []}],
     [{'color': []}, {'background': []}],
     [{'align': []}],
     ['clean'],
-    ['link', 'image', 'video']
+    ['link', 'image', 'rotate', 'video'],
   ]
-//
-  let fileBtn = document.getElementById('inputImg')
   let myQuill //增加一个全局quill，代表当前quill实例
-  let thisVue  //增加一个全局this，表示当前vue实例
 
   export default {
     name: "post",
@@ -75,19 +56,23 @@
               handlers: {
                 // 拦截image的click事件
                 'image': function () {
-                  myQuill = this.quill //全局quill源自此处
-                  let c = document.getElementById("inputImg");
-                  c.click()
-                }
-              }
 
+                  let c = document.getElementById("inputImg")
+                  c.click()
+                },
+                'rotate': function ($event) { // 旋转图片
+                  console.log("Rotate in !")
+
+                  console.log($event)
+                },
+              }
             },
             history: {
               delay: 1000,
               maxStack: 50,
               userOnly: false
             },
-            imageDrop: true,
+            //imageDrop: true,
             imageResize: {
               displayStyles: {
                 backgroundColor: 'black',
@@ -103,6 +88,11 @@
       }
     },
     methods: {
+      handleClick: function (evt) {
+        if (evt.target && evt.target.tagName && evt.target.tagName.toUpperCase() === 'IMG') {
+          console.log(evt.target)
+        }
+      },
       onEditorReady: function (editor) { // 准备编辑器
       },
       onEditorBlur: function () { // 失去焦点事件
@@ -111,36 +101,21 @@
       },
       onEditorChange: function () { // 内容改变事件
       },
-      insertImage: function () { // 内容改变事件
-
-
-      },
       onInputImgChange: function (event) {
-        console.log("in")
-
-
         let file = document.getElementById("inputImg").files[0]
-
         if (window.FileReader) {
-
           let fr = new FileReader()
           fr.readAsDataURL(file) //开始加载文件
-          fr.onload = function (e) { //文件加载结束，this = e.target
-            //返回FileReader对象：事件，状态，属性，结果等
-
+          fr.onload = function (e) { //文件加载结束，this = e.target,返回FileReader对象：事件，状态，属性，结果等
             let originImg = new Image()
             originImg.src = e.target.result //开始将结果（base64格式）加载到image
-
             originImg.onload = function () { //原始图片加载结束，this = 原始图片
               let w_old = this.width, h_old = this.height
               const w_new = 1024
               let h_new = (w_new * parseInt(h_old)) / parseInt(w_old)
-
-
               let canvas = document.createElement("canvas")
               canvas.width = w_new
               canvas.height = h_new
-
               if (w_old <= 1024) {
                 canvas.width = w_old
                 canvas.height = h_old
@@ -151,19 +126,17 @@
                 canvas.getContext("2d").drawImage(this, 0, 0, w_old, h_old, 0, 0, w_new, h_new)  //超宽图强制设为1024
               }
               let src = canvas.toDataURL("image/jpeg")
-              //此处不能直接使用this来访问vue实例，因为此时的this = originImg，因此此处采用全局quill来访问quill实例
+              //此处不能直接使用this（this = originImg）来访问vue实例，用全局quill来访问quill实例
               let length = myQuill.selection.savedRange.index // 获取光标所在位置
-              myQuill.insertEmbed(length, 'image', src)
-              // 调整光标到最后
-              myQuill.setSelection(length + 1)
-
+              myQuill.insertEmbed(length, 'image', src)       //插入图片
+              myQuill.setSelection(length + 1)               // 调整光标到最后
             }
-
           }
         } else {
           alert('暂不支持FileReader')
         }
       },
+
 
       saveHtml: function () {
 
@@ -178,94 +151,23 @@
         const regex = /<img src="(.+?)">/g;
         let m = regex.exec(this.content)
 
-        let img2 = document.getElementById("img2")
-
-
-        img2.setAttribute('src', m[1]);
-
-        this.yyy(img2)
-
-
         return
         this.axios.post('http://localhost:8081/setNewPost', param)
           .then((response) => {
             this.totalPage = JSON.parse(response.data)
           })
       },
-      imgToCanvas: function (image) {
-        let canvas = document.createElement("canvas");
-        canvas.width = image.width;
-        canvas.height = image.height;
-        canvas.getContext("2d").drawImage(image, 0, 0);
-        return canvas;
+      initRotateButton: function () {      //在quill中新增一个旋转图片的按钮
+        const sourceEditorButton = document.querySelector('.ql-rotate');
+        sourceEditorButton.style.cssText = "border:0px"
+        sourceEditorButton.innerHTML = "<img src='/static/rotate.png'>"
+        sourceEditorButton.title = "旋转图片"
       },
-      //canvas转换为image
-      canvasToImg: function (canvas) {
-        let array = ["image/webp", "image/jpeg", "image/png"],
-          type = document.getElementById('myselect').value - 1;
-        let src = canvas.toDataURL(array[type]);
-        return src;
-      },
-      //获取图片信息
-      getImg: function getImg(fn) {
-        let imgFile = new FileReader();
-        try {
-          imgFile.onload = function (e) {
-            let image = new Image();
-            image.src = e.target.result; //base64数据
-            image.onload = function () {
-              fn(image);
-            }
-          }
-          imgFile.readAsDataURL(document.getElementById('inputImg').files[0]);
-        } catch (e) {
-          console.log("请上传图片！" + e);
-        }
-      },
-      xxx: function () {
-        this.getImg(function (image) {
-          console.log(image)
-          let canvas = document.createElement("canvas");
-          canvas.width = image.width;
-          canvas.height = image.height;
-          canvas.getContext("2d").drawImage(image, 0, 0);
-
-          let imgshow = document.getElementById("imgShow")
-
-          let array = ["image/webp", "image/jpeg", "image/png"],
-            type = document.getElementById('myselect').value - 1;
-          let src = canvas.toDataURL(array[type]);
-
-
-          imgshow.setAttribute('src', src);
-        });
-      },
-      yyy: function (image) {
-        console.log(image)
-        let canvas = document.createElement("canvas");
-        //canvas.width = image.width;
-        //canvas.height = image.height;
-        let scale = image.width / image.height;
-
-
-        canvas.getContext("2d").drawImage(image, 0, 0);
-
-        let imgshow = document.getElementById("imgShow")
-
-        let array = ["image/webp", "image/jpeg", "image/png"],
-          type = document.getElementById('myselect').value - 1;
-        let src = canvas.toDataURL(array[type]);
-
-
-        imgshow.setAttribute('src', src);
-      },
-      zzz: function () {
-        console.log("hi")
-      }
     },
     mounted() {
-      thisVue = this
-
+      this.initRotateButton() //初始化自定义按钮
+      myQuill = this.$refs.myQuillEditor.quill //全局quill实例
+      myQuill.root.addEventListener('click', this.handleClick, false) //为全局quill创建一个根监听
     },
   }
 
